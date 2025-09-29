90139e0e6cee6218ed5f0aa387d1cf9b6b800ad0
geniusjun <geniusjun4663@gmail.com>
2025-09-29 18:40:07 +0900

    9/29 기능 목록 첫 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("8기 합격하자!");
+        List<Integer> list = Randoms.shuffle(Arrays.asList(1,2,3,4,5));
+        System.out.println(list);
+
     }
 }
