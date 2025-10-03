2826334ba07aa0145553aab60ca104b0ce896a2e
geniusjun <geniusjun4663@gmail.com>
2025-10-03 15:54:07 +0900

    refactor/5 : 에러 흐름 완성

--- src/main/java/baseball/view/ui/Input.java
+++ src/main/java/baseball/view/ui/Input.java
@@ -1,5 +1,27 @@
 package baseball.view.ui;
 
-public class Input {
+import baseball.global.exception.CustomException;
+import baseball.global.exception.ErrorMessage;
+import camp.nextstep.edu.missionutils.Console;
 
+public final class Input {
+
+    public static String Reader() {
+        return Console.readLine();
+    }
+
+    public static class Validator {
+        public static String validate(String message) {
+            ValidateInputBlank(message);
+            return message;
+        }
+
+        private static void ValidateInputBlank(String message) {
+
+            if (message.isBlank()) {
+                throw CustomException.from(ErrorMessage.BLANK_INPUT_ERROR);
+            }
+
+        }
+    }
 }
